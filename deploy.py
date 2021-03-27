1bda3d2132a190ddffb9c4a9eae2b4b828208d8e
ousttrue <ousttrue@gmail.com>
2021-03-27 23:37:00 +0900

    deploy.py for windows

--- deploy.py
+++ deploy.py
@@ -2,17 +2,24 @@
 import pathlib
 from typing import Optional
 import os
+import shutil
 
 SELF = pathlib.Path(__file__).absolute()
 DOTFILES = SELF.parent
 
-EXCLUDE = [SELF, DOTFILES / 'README.md', DOTFILES / 'deploy.sh', DOTFILES / 'scripts']
+EXCLUDE = [
+    SELF, DOTFILES / 'README.md', DOTFILES / 'deploy.sh', DOTFILES / 'scripts'
+]
 EXCLUDE_NAMES = ['.git', '.vscode']
 
 
 def get_home() -> pathlib.Path:
     if os.name == 'nt':
-        user_profile = os.environ['USER_PROFILE']
+        user_profile = os.environ['USERPROFILE']
+        if user_profile:
+            return pathlib.Path(user_profile)
+        else:
+            raise Exception('no USER_PROFILE')
 
     else:
         home = os.environ['HOME']
@@ -25,23 +32,40 @@ def get_home() -> pathlib.Path:
 HOME = get_home()
 
 
-def deploy_file(src: pathlib.Path, dst: pathlib.Path):
-    if not src.parent.exists():
-        src.parent.mkdir()
+def deploy_nt(src: pathlib.Path, dst: pathlib.Path):
+    if dst.exists():
+        if src.read_bytes() == dst.read_bytes():
+            # same contents
+            return
+        else:
+            print(f'exists: {dst}')
+            return
+    else:
+        print(f'copy: {src} => {dst}')
+        shutil.copy(src, dst)
 
+
+def deploy_posix(src: pathlib.Path, dst: pathlib.Path):
     if dst.exists():
         if dst.is_symlink():
             return
         else:
             print(f'exists: {dst}')
             return
-
-    if os.name == 'nt':
-        print(f'copy: {src} => {dst}')
-
     else:
         print(f'link: {src} => {dst}')
         dst.symlink_to(src)
+
+
+def deploy_file(src: pathlib.Path, dst: pathlib.Path):
+    if not dst.parent.exists():
+        dst.parent.mkdir()
+
+    if os.name == 'nt':
+        deploy_nt(src, dst)
+
+    else:
+        deploy_posix(src, dst)
 
 
 def deploy_dir(root: pathlib.Path, current: Optional[pathlib.Path] = None):
