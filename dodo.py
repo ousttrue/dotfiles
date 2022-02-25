e79cfb74b47e2f1d0e0eae93cadd5c6056b5832f
ousttrue <ousttrue@gmail.com>
2022-02-25 16:53:29 +0900

    resolve

--- dodo.py
+++ dodo.py
@@ -2,6 +2,7 @@ import pathlib
 import os
 HERE = pathlib.Path(__file__).absolute().parent
 SYNC_DIR = HERE / 'sync'
+SYNC_HOME_DIR = SYNC_DIR / 'HOME'
 
 
 def get_home() -> pathlib.Path:
@@ -17,11 +18,20 @@ def get_home() -> pathlib.Path:
 HOME_DIR = get_home()
 
 
-def mklink(src: pathlib.Path, dst: pathlib.Path):
+def mklink(dependencies, targets):
+    src = pathlib.Path(dependencies[0])
+    dst = pathlib.Path(targets[0])
+    print(src, dst)
     if dst.exists():
         print(f'rm {dst}')
         dst.unlink()
-    dst.symlink_to(src)
+    dst.parent.mkdir(exist_ok=True, parents=True)
+    assert(src.is_file())
+    dst.symlink_to(src, target_is_directory=False)
+
+
+def check_link(src: pathlib.Path, dst: pathlib.Path):
+    return dst.is_symlink() and dst.resolve() == src
 
 
 def task_create_link():
@@ -29,9 +39,12 @@ def task_create_link():
     create symbol link for config files
     '''
     target = '.xonshrc'
-    src = SYNC_DIR / target
+    src = SYNC_HOME_DIR / target
     dst = HOME_DIR / target
-    return {        
-        'actions': [(mklink, (dst, dst))],
+    return {
+        'file_dep': [src],
+        'targets': [dst],
+        'actions': [(mklink)],
+        'uptodate': [(check_link, (src, dst))],
         'verbosity': 2,
     }
