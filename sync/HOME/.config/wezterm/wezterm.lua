35035e28ef46afbc9b3ed79aa7f022a8d61e2b37
ousttrue <ousttrue@gmail.com>
2022-03-27 23:30:56 +0900

    wezterm.lua

--- sync/HOME/.config/wezterm/wezterm.lua
+++ sync/HOME/.config/wezterm/wezterm.lua
@@ -1,3 +1,12 @@
-return {
+local wezterm = require("wezterm")
 
+return {
+    default_prog = {"C:/Python310/Scripts/xonsh.exe"},
+    font = wezterm.font("Cica"),
+    font_size = 14.0,
+    color_scheme = "OneHalfDark",
+    hide_tab_bar_if_only_one_tab = true,
+    leader = { key="t", mods="CTRL", timeout_milliseconds=1000 },
+    keys = {
+    },
 }
