2a454612ff4b2a926706aaea2f15acf959ecbcdf
ousttrue <ousttrue@gmail.com>
2022-03-28 21:33:49 +0900

    stylua

--- sync/HOME/.config/nvim/init.vim
+++ sync/HOME/.config/nvim/init.vim
@@ -5,8 +5,9 @@ augroup packer_user_config
     autocmd BufWritePost plugins.lua source <afile> | PackerCompile
 augroup end
 
-
 set ts=4 sts=4 sw=4 expandtab
+set list
+set listchars=eol:$,tab:>-,trail:~,extends:>,precedes:<
 set laststatus=2
 set belloff=all
 set noswapfile noundofile nobackup
--- sync/HOME/.config/nvim/lua/plugins.lua
+++ sync/HOME/.config/nvim/lua/plugins.lua
@@ -3,60 +3,61 @@
 -- Only required if you have packer configured as `opt`
 local fn = vim.fn
 
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
+    packer_bootstrap = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    }
 end
 
-vim.cmd([[packadd packer.nvim]])
+vim.cmd [[packadd packer.nvim]]
 
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
-	use("tpope/vim-fugitive")
-	use("airblade/vim-gitgutter")
-	use({
-		"kyazdani42/nvim-tree.lua",
-		requires = {
-			"kyazdani42/nvim-web-devicons", -- optional, for file icon
-		},
-		config = function()
-			vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true })
-			require("nvim-tree").setup({})
-		end,
-	})
+    use "wbthomason/packer.nvim"
+    use "tpope/vim-fugitive"
+    use "airblade/vim-gitgutter"
+    use {
+        "kyazdani42/nvim-tree.lua",
+        requires = {
+            "kyazdani42/nvim-web-devicons", -- optional, for file icon
+        },
+        config = function()
+            vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true })
+            require("nvim-tree").setup {}
+        end,
+    }
 
-	use("neovim/nvim-lspconfig") -- Collection of configurations for the built-in LSP client
+    use "neovim/nvim-lspconfig" -- Collection of configurations for the built-in LSP client
 
-	-- use "itchyny/lightline.vim"
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-		config = function()
-			require("lualine").setup()
-		end,
-	})
+    -- use "itchyny/lightline.vim"
+    use {
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+        config = function()
+            require("lualine").setup()
+        end,
+    }
 
-	use({
-		"tpope/vim-commentary",
-		config = function()
-			vim.api.nvim_set_keymap("n", "<C-_>", ":Commentary<CR>", { noremap = true })
-			vim.api.nvim_set_keymap("", "<C-_>", ":Commentary<CR>", { noremap = true })
-		end,
-	})
+    use {
+        "tpope/vim-commentary",
+        config = function()
+            vim.api.nvim_set_keymap("n", "<C-_>", ":Commentary<CR>", { noremap = true })
+            vim.api.nvim_set_keymap("", "<C-_>", ":Commentary<CR>", { noremap = true })
+        end,
+    }
 
-	use({
-		"sbdchd/neoformat",
-		config = function()
-			vim.api.nvim_set_keymap("n", "<S-f>", ":Neoformat<CR>", { noremap = true })
-			vim.api.nvim_set_keymap("v", "<S-f>", ":Neoformat<CR>", { noremap = true })
-		end,
-	})
+    use {
+        "sbdchd/neoformat",
+        config = function()
+            vim.api.nvim_set_keymap("n", "<S-f>", ":Neoformat<CR>", { noremap = true })
+            vim.api.nvim_set_keymap("v", "<S-f>", ":Neoformat<CR>", { noremap = true })
+            vim.api.nvim_set_var("neoformat_basic_format_retab", "1")
+        end,
+    }
 end)
