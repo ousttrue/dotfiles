b1a25694a9b501a12797c9798d8ade1b5235fe16
ousttrue <ousttrue@gmail.com>
2022-03-31 23:43:05 +0900

    wezterm tab

--- sync/HOME/.config/wezterm/wezterm.lua
+++ sync/HOME/.config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require "wezterm"
 
 local config = {
+    use_ime = true,
     default_prog = { "xonsh" },
     initial_cols = 90,
     initial_rows = 50,
@@ -65,5 +66,38 @@ else
     config.default_prog = { "xonsh" }
     table.insert(config.launch_menu, { label = "zsh", args = { "zsh", "-l" } })
 end
+
+local function basename(s)
+    return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+
+local function convert_home_dir(path)
+    local cwd = path
+    local home = os.getenv "HOME"
+    cwd = cwd:gsub("^" .. home .. "/", "~/")
+    return cwd
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+    local user_title = tab.active_pane.user_vars.panetitle
+    if user_title ~= nil and #user_title > 0 then
+        return {
+            { Text = tab.tab_index + 1 .. ":" .. user_title },
+        }
+    end
+
+    local title = wezterm.truncate_right(basename(tab.active_pane.foreground_process_name), max_width)
+    if title == "" then
+        local uri = convert_home_dir(tab.active_pane.current_working_dir)
+        local name = basename(uri)
+        if name == "" then
+            name = uri
+        end
+        title = wezterm.truncate_right(name, max_width)
+    end
+    return {
+        { Text = tab.tab_index + 1 .. ":" .. title },
+    }
+end)
 
 return config
