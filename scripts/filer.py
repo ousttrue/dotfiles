93abb5b3de41ff1e055cee2b1c88605ecf392266
ousttrue <ousttrue@gmail.com>
2022-04-05 19:15:58 +0900

    padding

--- scripts/filer.py
+++ scripts/filer.py
@@ -1,6 +1,7 @@
-from typing import List, Tuple, Iterable
+from typing import List, Tuple, Iterable, Optional
 import asyncio
 import pathlib
+from wcwidth import wcswidth
 from prompt_toolkit.application.current import get_app
 import prompt_toolkit
 import prompt_toolkit.layout
@@ -23,12 +24,14 @@ STYLE = prompt_toolkit.styles.Style.from_dict({
 class FileSelector(prompt_toolkit.layout.FormattedTextControl):
     def __init__(self, kb: prompt_toolkit.key_binding.KeyBindings, current: pathlib.Path) -> None:
         super().__init__(
-            lambda: self.text,
+            self._get_text,
             focusable=True,
             get_cursor_position=lambda: prompt_toolkit.data_structures.Point(0, self.selected))
-        self.text: List[Tuple[str, str]] = []
+        self._text_cache: Optional[List[Tuple[str, str]]] = None
         self.selected = 0
+        self.max_wc_width = 0
 
+        self.invalidated = prompt_toolkit.utils.Event(self)
         self.dir = None
         self.chdir(current)
 
@@ -39,7 +42,8 @@ class FileSelector(prompt_toolkit.layout.FormattedTextControl):
         self._keybind(self.down, 'down')
         self._keybind(self.down, 'j')
 
-        self.on_selected = prompt_toolkit.utils.Event(self)
+    def get_invalidate_events(self) -> Iterable[prompt_toolkit.utils.Event[object]]:
+        return [self.invalidated]
 
     def create_content(self, *args):
         content = super().create_content(*args)
@@ -57,14 +61,39 @@ class FileSelector(prompt_toolkit.layout.FormattedTextControl):
     def _keybind(self, callback, *args, **kw):
         self.kb.add(*args, **kw)(callback)
 
+    def _get_text(self) -> List[Tuple[str, str]]:
+        assert(self.dir)
+        if not isinstance(self._text_cache, list):
+            items = [(f, wcswidth(f.name)) for f in self.dir.iterdir()]
+            max_wc = max(l for f, l in items)
+
+            def wc_padding(text, l):
+                return text + ' ' * max(0, (max_wc - l))
+
+            def to_text(f: pathlib.Path, l: int) -> Tuple[str, str]:
+                if f.is_dir():
+                    return ('class:dir', ' ' + wc_padding(f.name, l) + '\n')
+                else:
+                    return ('class:file', ' ' + wc_padding(f.name, l) + '\n')
+
+            self._text_cache = []
+            for f, l in items:
+                fragment = to_text(f, l)
+                w = wcswidth(fragment[1])
+                if w > self.max_wc_width:
+                    self.max_wc_width = w
+                self._text_cache.append(fragment)
+
+        return self._text_cache
+
     def select(self, selected: int):
         if self.selected == selected:
             return
-        if selected < 0 or selected >= len(self.text):
+        if not self._text_cache or selected < 0 or selected >= len(self._text_cache):
             return
 
         self.selected = selected
-        self.on_selected.fire()
+        self.invalidated.fire()
 
     def up(self, e):
         self.select(self.selected-1)
@@ -75,6 +104,7 @@ class FileSelector(prompt_toolkit.layout.FormattedTextControl):
     def go_parent(self, e: prompt_toolkit.key_binding.KeyPressEvent):
         assert(self.dir)
         self.chdir(self.dir.parent)
+        self.select(0)
 
     def chdir(self, dir: pathlib.Path):
         dir = dir.absolute()
@@ -82,14 +112,7 @@ class FileSelector(prompt_toolkit.layout.FormattedTextControl):
             return
         assert(dir.is_dir())
         self.dir = dir
-
-        def to_text(f: pathlib.Path) -> Tuple[str, str]:
-            if f.is_dir():
-                return ('class:dir', f' {f.name}\n')
-            else:
-                return ('class:file', f' {f.name}\n')
-
-        self.text = [to_text(f) for f in self.dir.iterdir()]
+        self._text_cache = None
 
 
 async def main():
@@ -99,12 +122,12 @@ async def main():
     def quit(e: prompt_toolkit.key_binding.KeyPressEvent):
         e.app.exit()
 
-    root = prompt_toolkit.layout.Window(
-        FileSelector(kb, pathlib.Path('.')))
+    selector = FileSelector(kb, pathlib.Path('.'))
+    root = prompt_toolkit.layout.Window(selector)
 
     app = prompt_toolkit.Application(
         full_screen=True,
-        layout=prompt_toolkit.layout.Layout(root, root),
+        layout=prompt_toolkit.layout.Layout(root, selector),
         key_bindings=kb,
         style=STYLE,
         editing_mode=prompt_toolkit.enums.EditingMode.VI,
