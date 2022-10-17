43b4eac1534cd5c193adc713f7ff503d90281976
ousttrue <ousttrue@gmail.com>
2022-10-17 22:10:35 +0900

    update

--- sync/HOME/.config/nvim/init.lua
+++ sync/HOME/.config/nvim/init.lua
@@ -46,8 +46,8 @@ autocmd QuickfixCmdPost make,grep,grepadd,vimgrep cwindow
 ]]
 
 -- vim.keymap.set("n", "q", ":close<CR>", { noremap = true })
-vim.keymap.set("n", "[", ":bp<CR>", { noremap = true })
-vim.keymap.set("n", "]", ":bn<CR>", { noremap = true })
+vim.keymap.set("n", "[b", ":bp<CR>", { noremap = true })
+vim.keymap.set("n", "]b", ":bn<CR>", { noremap = true })
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
 
 -- lsp
@@ -56,11 +56,13 @@ vim.keymap.set("v", "<S-f>", vim.lsp.buf.format, { noremap = true })
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true })
 vim.keymap.set("n", "gf", vim.lsp.buf.format, { noremap = true })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { noremap = true })
+vim.keymap.set("n", "<f12>", vim.lsp.buf.references, { noremap = true })
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true })
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { noremap = true })
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { noremap = true })
 vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { noremap = true })
 vim.keymap.set("n", "gn", vim.lsp.buf.rename, { noremap = true })
+vim.keymap.set("n", "<f2>", vim.lsp.buf.rename, { noremap = true })
 vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { noremap = true })
 vim.keymap.set("n", "ge", vim.diagnostic.open_float, { noremap = true })
 vim.keymap.set("n", "g]", vim.diagnostic.goto_next, { noremap = true })
