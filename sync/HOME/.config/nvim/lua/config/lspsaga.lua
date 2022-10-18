207a2a62c87987aa3eb2109d50290a11b8c58a68
ousttrue <ousttrue@gmail.com>
2022-10-19 01:57:37 +0900

    update

--- sync/HOME/.config/nvim/lua/config/lspsaga.lua
+++ sync/HOME/.config/nvim/lua/config/lspsaga.lua
@@ -1,8 +1,14 @@
 local M = {}
 
 function M.setup()
-    local lspsaga = require "lspsaga"
-    lspsaga.setup {}
+    local saga = require "lspsaga"
+
+    saga.init_lsp_saga {
+        -- your configuration
+    }
+
+    local keymap = vim.keymap.set
+    keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
 end
 
 return M
--- sync/HOME/.config/nvim/lua/config/nvim-lspconfig.lua
+++ sync/HOME/.config/nvim/lua/config/nvim-lspconfig.lua
@@ -1,60 +1,73 @@
 local M = {}
 function M.setup()
-    local lspconfig = require "lspconfig"
-    local lsp_status = require "lsp-status"
-    local aerial = require "aerial"
-    local symbols_outline = require "symbols-outline"
-    lsp_status.register_progress()
+  local lspconfig = require "lspconfig"
+  local lsp_status = require "lsp-status"
+  local aerial = require "aerial"
+  local symbols_outline = require "symbols-outline"
+  lsp_status.register_progress()
 
-    local function on_attach(client, bufnr)
-        aerial.on_attach(client, bufnr)
-        lsp_status.on_attach(client, bufnr)
-        symbols_outline.open_outline()
-    end
+  local function on_attach(client, bufnr)
+    aerial.on_attach(client, bufnr)
+    lsp_status.on_attach(client, bufnr)
+    -- symbols_outline.open_outline()
+  end
 
-    -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    for k, v in pairs(lsp_status.capabilities) do
-        capabilities[k] = v
-    end
+  -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+  local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  for k, v in pairs(lsp_status.capabilities) do
+    capabilities[k] = v
+  end
 
-    lspconfig.sumneko_lua.setup {
-        settings = {
-            Lua = {
-                diagnostics = {
-                    enable = true,
-                    globals = { "vim" },
-                },
-            },
+  lspconfig.sumneko_lua.setup {
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = "LuaJIT",
         },
-        on_attach = on_attach,
-        capabilities = capabilities,
-    }
-
-    lspconfig.clangd.setup {
-        handlers = lsp_status.extensions.clangd.setup(),
-        init_options = {
-            clangdFileStatus = true,
+        diagnostics = {
+          enable = true,
+          globals = { "vim" },
         },
-        on_attach = on_attach,
-        capabilities = lsp_status.capabilities,
-    }
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
 
-    lspconfig.pyright.setup {
-        on_attach = on_attach,
-        capabilities = lsp_status.capabilities,
-    }
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
 
-    lspconfig.zls.setup {
-        cmd = { vim.env.YAZLS_EXE },
-        on_attach = on_attach,
-        capabilities = lsp_status.capabilities,
-    }
+  lspconfig.clangd.setup {
+    handlers = lsp_status.extensions.clangd.setup(),
+    init_options = {
+      clangdFileStatus = true,
+    },
+    on_attach = on_attach,
+    capabilities = lsp_status.capabilities,
+  }
 
-    lspconfig.zk.setup {
-        on_attach = on_attach,
-        capabilities = lsp_status.capabilities,
-    }
+  lspconfig.pyright.setup {
+    on_attach = on_attach,
+    capabilities = lsp_status.capabilities,
+  }
+
+  lspconfig.zls.setup {
+    cmd = { vim.env.YAZLS_EXE },
+    on_attach = on_attach,
+    capabilities = lsp_status.capabilities,
+  }
+
+  lspconfig.zk.setup {
+    on_attach = on_attach,
+    capabilities = lsp_status.capabilities,
+  }
 end
 
 return M
