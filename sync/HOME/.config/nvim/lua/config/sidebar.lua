772a003d64a1d477be55edf05e0dcae6fafe7669
ousttrue <ousttrue@gmail.com>
2022-10-19 02:51:59 +0900

    SymbolsOutline

--- sync/HOME/.config/nvim/lua/config/sidebar.lua
+++ sync/HOME/.config/nvim/lua/config/sidebar.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
     local sidebar = require "sidebar-nvim"
     sidebar.setup()
-    vim.keymap.set("n", "<Leader>s", ":SidebarNvimToggle<CR>", { noremap = true })
+    -- vim.keymap.set("n", "<Leader>s", ":SidebarNvimToggle<CR>", { noremap = true })
 end
 
 return M
--- sync/HOME/.config/nvim/lua/config/symbols-outline.lua
+++ sync/HOME/.config/nvim/lua/config/symbols-outline.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.setup()
   require("symbols-outline").setup()
+  vim.keymap.set("n", "<Leader>s", ":SymbolsOutline<CR>", { noremap = true })
 end
 
 return M
