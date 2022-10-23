990e3b5ae20079986278720a106702aedaf6864c
ousttrue <ousttrue@gmail.com>
2022-10-23 10:14:45 +0900

    conf

--- sync/HOME/.config/nvim/LuaSnip/sh.lua
+++ sync/HOME/.config/nvim/LuaSnip/sh.lua
@@ -1,6 +1,6 @@
 return { -- Shorthand example: the same snippet as above, but only setting the `trig` param
   s(
-    "CONFIGURE", -- the snip_param table is replaced by a single string holding `trig`
+    "CONF", -- the snip_param table is replaced by a single string holding `trig`
     { -- Table 2: snippet nodes
       t "cmake -B build -S . -DEXPORT_COMPILE_COMMANDS=1",
     }
