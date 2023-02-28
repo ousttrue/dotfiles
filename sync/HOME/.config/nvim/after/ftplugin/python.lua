a81a0ff6eee1a153b86fe4de13f78da627951351
ousttrue <ousttrue@gmail.com>
2023-02-28 14:04:39 +0900

    python on Linux

--- sync/HOME/.config/nvim/after/ftplugin/python.lua
+++ sync/HOME/.config/nvim/after/ftplugin/python.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "F", "<cmd>Neoformat python black<CR>",
-  { buffer = 0, noremap = true, silent = true })
-vim.keymap.set("x", "F", "<cmd>Neoformat! python black<CR>",
-  { buffer = 0, noremap = true, silent = true })
+-- vim.keymap.set("n", "F", "<cmd>Neoformat python black<CR>",
+--   { buffer = 0, noremap = true, silent = true })
+-- vim.keymap.set("x", "F", "<cmd>Neoformat! python black<CR>",
+--   { buffer = 0, noremap = true, silent = true })
