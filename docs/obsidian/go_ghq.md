c33a63b2f055b5e74fc32a2a6ee69fb1cd1cae09
ousttrue <ousttrue@gmail.com>
2023-04-11 22:27:54 +0900

    update

---
+++ docs/obsidian/go_ghq.md
@@ -0,0 +1,4 @@
+[[golang]]
+
+# maintenance
+- [ghq で管理しているリポジトリを複数選択して消す ghq-unget コマンド書いた - Qiita](https://qiita.com/uasi/items/dae9b180680e90950cb1)
--- sync/HOME/.config/nyagos/nyagos_init.lua
+++ sync/HOME/.config/nyagos/nyagos_init.lua
@@ -45,6 +45,10 @@ function M.setup()
     end
   end
 
+  function nyagos.alias.tig(args)
+    nyagos.exec '"C:/Program Files/Git/usr/bin/tig"'
+  end
+
   local function search_history(this, is_prev)
     -- カーソル位置が一番左の場合は通常のnext/prev
     if this.pos == 1 then
