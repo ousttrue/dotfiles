758410492d5b02de864befabedaf9a7f367fe7f7
ousttrue <ousttrue@gmail.com>
2023-08-28 03:24:15 +0900

    update

--- sync/HOME/lua/nya/dotfiles.lua
+++ sync/HOME/lua/nya/dotfiles.lua
@@ -1,20 +1,37 @@
-local M = {}
+local M = {
+  download = {
+    ["~/.skk/SKK-JISYO.L"] = "https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.L",
+    ["~/.skk/SKK-JISYO.emoji"] = "https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.emoji",
+    -- font: HackgenNerd
+    -- zig
+    -- fzf
+  },
+  build = {
+    -- nvim
+    -- chafa
+    -- gtk4
+    -- gstreamer
+  },
+  go = {
+    -- ghq
+    -- lazygit
+  },
+  cargo = {
+    -- zoxide
+    -- exa
+    -- lsd
+  },
+}
 
 local NYA = require "nya.util"
 local ES = require "common.escape_seuquence"
-
-M.data = {
-  ["~/.skk/SKK-JISYO.L"] = "https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.L",
-  ["~/.skk/SKK-JISYO.emoji"] = "https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.emoji",
-}
 
 function M.setup()
   function nyagos.alias.dotfile(args)
     local cmd = unpack(args.rawargs)
     -- print(args.rawargs)
-    -- TODO: gum 使う
-    if cmd == "data" then
-      for k, v in pairs(M.data) do
+    if cmd == "download" then
+      for k, v in pairs(M.download) do
         if NYA.is_exists(k) then
           nyagos.write(ES.format("%s: <green>OK<default>\n", k))
         else
