c82870f94eee4ebce51f9ecef6c3d01f75274426
ousttrue <ousttrue@gmail.com>
2024-08-27 02:27:56 +0900

    update

--- nvimruntime/lua/lazy_plugins/00_minimum.lua
+++ nvimruntime/lua/lazy_plugins/00_minimum.lua
@@ -3,70 +3,70 @@
 --
 -- config > opts
 return {
-  {
-    "folke/edgy.nvim",
-    event = "VeryLazy",
-    opts = {
-      animate = {
-        enabled = false,
-      },
-      bottom = {
-        -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
-        {
-          ft = "toggleterm",
-          size = { height = 0.4 },
-          -- exclude floating windows
-          filter = function(buf, win)
-            return vim.api.nvim_win_get_config(win).relative == ""
-          end,
-        },
-        {
-          ft = "lazyterm",
-          title = "LazyTerm",
-          size = { height = 0.4 },
-          filter = function(buf)
-            return not vim.b[buf].lazyterm_cmd
-          end,
-        },
-        "Trouble",
-        {
-          ft = "qf",
-          title = "QuickFix",
-          size = { height = 14 },
-        },
-        {
-          ft = "help",
-          size = { height = 20 },
-          -- only show help buffers
-          filter = function(buf)
-            return vim.bo[buf].buftype == "help"
-          end,
-        },
-        { ft = "spectre_panel", size = { height = 0.4 } },
-      },
-      left = {
-        -- Neo-tree filesystem always takes half the screen height
-        {
-          title = "Neo-Tree",
-          ft = "neo-tree",
-          filter = function(buf)
-            return vim.b[buf].neo_tree_source == "filesystem"
-          end,
-          size = { height = 0.5 },
-        },
-        -- any other neo-tree windows
-        "neo-tree",
-      },
-      right = {
-        {
-          title = "Aerial",
-          ft = "aerial",
-          pinned = true,
-          -- open = "SymbolsOutlineOpen",
-        },
-      },
-    },
-  },
+  -- {
+  --   "folke/edgy.nvim",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     animate = {
+  --       enabled = false,
+  --     },
+  --     bottom = {
+  --       -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
+  --       {
+  --         ft = "toggleterm",
+  --         size = { height = 0.4 },
+  --         -- exclude floating windows
+  --         filter = function(buf, win)
+  --           return vim.api.nvim_win_get_config(win).relative == ""
+  --         end,
+  --       },
+  --       {
+  --         ft = "lazyterm",
+  --         title = "LazyTerm",
+  --         size = { height = 0.4 },
+  --         filter = function(buf)
+  --           return not vim.b[buf].lazyterm_cmd
+  --         end,
+  --       },
+  --       "Trouble",
+  --       {
+  --         ft = "qf",
+  --         title = "QuickFix",
+  --         size = { height = 14 },
+  --       },
+  --       {
+  --         ft = "help",
+  --         size = { height = 20 },
+  --         -- only show help buffers
+  --         filter = function(buf)
+  --           return vim.bo[buf].buftype == "help"
+  --         end,
+  --       },
+  --       { ft = "spectre_panel", size = { height = 0.4 } },
+  --     },
+  --     left = {
+  --       -- Neo-tree filesystem always takes half the screen height
+  --       {
+  --         title = "Neo-Tree",
+  --         ft = "neo-tree",
+  --         filter = function(buf)
+  --           return vim.b[buf].neo_tree_source == "filesystem"
+  --         end,
+  --         size = { height = 0.5 },
+  --       },
+  --       -- any other neo-tree windows
+  --       "neo-tree",
+  --     },
+  --     right = {
+  --       {
+  --         title = "Aerial",
+  --         ft = "aerial",
+  --         pinned = true,
+  --         -- open = "SymbolsOutlineOpen",
+  --       },
+  --     },
+  --   },
+  -- },
   {
     "nil70n/floating-help",
     opts = {
