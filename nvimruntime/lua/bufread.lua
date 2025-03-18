2edd79b9ec7e4a086ccce2377d1949960773c55f
ousttrue <ousttrue@gmail.com>
2025-03-19 04:46:41 +0900

    update

--- nvimruntime/lua/bufread.lua
+++ nvimruntime/lua/bufread.lua
@@ -43,6 +43,75 @@ function TSNodePath:push(node)
   return TSNodePath.new(self.src, unpack(list))
 end
 
+---@class HtmlElement
+---@field src string
+---@field node TSNode
+---@field children HtmlElement[]
+local HtmlElement = {}
+HtmlElement.__index = HtmlElement
+
+---@param src string
+---@param node TSNode
+---@return HtmlElement
+function HtmlElement.new(src, node)
+  local node_type = node:type()
+  assert(node_type == "element" or node_type == "document")
+  local self = setmetatable({
+    src = src,
+    node = node,
+    children = {},
+  }, HtmlElement)
+  return self
+end
+
+---@param src string
+---@param node TSNode
+---@param parent HtmlElement?
+function HtmlElement.build(src, node, parent)
+  local element = HtmlElement.new(src, node)
+
+  for i = 0, node:named_child_count() - 1 do
+    local child = node:named_child(i)
+    if child then
+      if child:type() == "element" then
+        local child_elment = HtmlElement.build(src, child, element)
+        element:add_child(child_elment)
+      end
+    end
+  end
+
+  return element
+end
+
+function HtmlElement:add_child(child)
+  table.insert(self.children, child)
+end
+
+---@param lines string[]
+---@param indent string
+function HtmlElement:traverse(lines, indent)
+  table.insert(lines, ("%s%s"):format(indent, self))
+  for i, child in ipairs(self.children) do
+    child:traverse(lines, indent .. "  ")
+  end
+end
+
+function HtmlElement:__tostring()
+  for i = 0, self.node:named_child_count() - 1 do
+    local child = self.node:named_child(i)
+    if child and child:type() == "start_tag" then
+      for j = 0, self.node:named_child_count() - 1 do
+        local childchild = child:named_child(j)
+        if childchild and childchild:type() == "tag_name" then
+          return vim.treesitter.get_node_text(child, self.src):gsub("\n", " ")
+        end
+      end
+    end
+  end
+
+  return "<no_tag>"
+end
+
 local function push(path, node)
   local list = {}
   for _, p in ipairs(path) do
@@ -83,22 +152,86 @@ local function traverse(lines, path)
   end
 end
 
+---@class HtmlElementIterator
+---@field stack [HtmlElement, integer][]
+local HtmlElementIterator = {}
+HtmlElementIterator.__index = HtmlElementIterator
+
+---@param element HtmlElement
+---@return HtmlElementIterator
+function HtmlElementIterator.new(element)
+  local self = setmetatable({
+    stack = { { element, 1 } },
+  }, HtmlElementIterator)
+  return self
+end
+
+---@return string?
+function HtmlElement:text()
+  for i = 0, self.node:named_child_count() - 1 do
+    local child = self.node:named_child(i)
+    if child and child:type() == "text" then
+      return vim.treesitter.get_node_text(child, self.src)
+    end
+  end
+end
+
+---@return HtmlElement?
+function HtmlElementIterator:next()
+  while #self.stack > 0 do
+    local current, index = unpack(self.stack[#self.stack])
+    local next_elment = current.children[index]
+    if next_elment then
+      table.insert(self.stack, { next_elment, 1 })
+      return next_elment
+    else
+      table.remove(self.stack)
+      if #self.stack > 0 then
+        self.stack[#self.stack][2] = self.stack[#self.stack][2] + 1
+      end
+    end
+  end
+end
+
+function HtmlElement:iterator()
+  ---@param it HtmlElementIterator
+  local function f(it)
+    local element = it:next()
+    return element
+  end
+  return f, HtmlElementIterator.new(self)
+end
+
 ---@param lines string[]
 ---@param body string
 local function add_lines(lines, body)
---   body = [[
--- <html>
---   <body>
---     <p>hello</p>
---   </body>
--- </html>
--- ]]
+  -- body = [[
+  -- <html>
+  --   <body>
+  --     <p>hello</p>
+  --   </body>
+  -- </html>
+  -- ]]
   local parser = vim.treesitter.get_string_parser(body, "html")
   local tree = parser:parse()
   if tree then
     local root = tree[1]:root()
-    traverse(lines, TSNodePath.new(body, root))
+    -- traverse(lines, TSNodePath.new(body, root))
+    local element = HtmlElement.build(body, root)
+    -- element:traverse(lines, "")
+    for current in element:iterator() do
+      local text = current:text()
+      if text then
+        for _, l in ipairs(vim.split(text, "\n")) do
+          table.insert(lines, l)
+        end
+      end
+    end
   end
+
+  -- for _, l in ipairs(vim.split(body, "\n")) do
+  --   table.insert(lines, l)
+  -- end
 end
 
 ---@param lines string[]
@@ -124,8 +257,20 @@ function M.setup()
   vim.api.nvim_create_autocmd("BufReadCmd", {
     pattern = { "http://*", "https://*" },
     callback = function(ev)
+      vim.api.nvim_set_option_value("modifiable", true, { buf = ev.buf })
+
       -- print(string.format("event fired: %s", vim.inspect(ev)))
-      local dl_job = vim.system({ "curl", "-Li", ev.file }, { text = false }):wait()
+      local dl_job = vim
+        .system({
+          "curl",
+          "-0",
+          "-L",
+          "-i",
+          "-H",
+          "USER-AGENT: w3m/0.5.3+git20230121",
+          ev.file,
+        }, { text = false })
+        :wait()
       -- vim.notify_once(("get %dbytes"):format(#dl_job.stdout), vim.log.levels.INFO, { title = ev.file })
       local res = dl_job.stdout
       assert(res)
